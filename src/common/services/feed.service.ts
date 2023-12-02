8f409719521f85fe1de89726624f5c998a149508
rnimisha <nimisaraut@gmail.com>
2023-12-02 13:41:36 +0545

    feat: send page data

--- src/common/services/feed.service.ts
+++ src/common/services/feed.service.ts
@@ -3,9 +3,12 @@ import { FeedPostType } from '@/common/types';
 import { getErrorResponse } from '@/common/utils';
 import { AxiosError } from 'axios';
 
-export const getFeedPosts = async (): Promise<FeedPostType[]> => {
+export const getFeedPosts = async (
+  page?: number,
+  pageSize = 10
+): Promise<FeedPostType[]> => {
   try {
-    const resp = await apiClient.get('/feed');
+    const resp = await apiClient.get('/feed', { params: { page, pageSize } });
     return resp.data;
   } catch (error) {
     const err = getErrorResponse(error as AxiosError);
