642b8d2a2a8e162f55afc73bdb2f8ca4f03a8ae5
rnimisha <nimisaraut@gmail.com>
2023-12-02 13:44:17 +0545

    feat: use inifinte query

--- src/hooks/useFeedPost.tsx
+++ src/hooks/useFeedPost.tsx
@@ -1,16 +1,42 @@
 import { getFeedPosts } from '@/common/services';
 import { FeedPostType } from '@/common/types';
-import { useQuery } from '@tanstack/react-query';
+import {
+  InfiniteData,
+  QueryFunctionContext,
+  useInfiniteQuery,
+} from '@tanstack/react-query';
 
 type Props = {
-  onSuccess?: (data: FeedPostType[]) => void;
+  onSuccess?: (data: InfiniteData<FeedPostType[]>) => void;
   onError?: (err: unknown) => void;
 };
 
 const useFeedPost = ({ onSuccess, onError }: Props) =>
-  useQuery<FeedPostType[], Error>(['getMyFeedPost'], () => getFeedPosts(), {
-    onSuccess,
-    onError,
-  });
-
+  useInfiniteQuery<FeedPostType[], Error>(
+    ['getMyFeedPost'],
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async ({ pageParam = 1 }: QueryFunctionContext<any>) => {
+      const pageSize = 10;
+      // const delayedFetch = new Promise<FeedPostType[]>((resolve) => {
+      //   setTimeout(async () => {
+      //     const data = await getFeedPosts(pageParam, pageSize);
+      //     resolve(data);
+      //   }, 1000);
+      // });
+      // return delayedFetch;
+      const data = await getFeedPosts(pageParam, pageSize);
+      return data;
+    },
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const pageSize = 10;
+        if (lastPage.length < pageSize) {
+          return undefined;
+        }
+        return allPages.length + 1;
+      },
+      onSuccess,
+      onError,
+    }
+  );
 export default useFeedPost;
