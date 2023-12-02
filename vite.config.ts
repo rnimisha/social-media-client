7484adb461fd714a8c45910f4877a2e3ace222ec
rnimisha <nimisaraut@gmail.com>
2023-12-02 19:56:56 +0545

    chore: change port for the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3007,
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      "@":path.resolve(__dirname, "./src")
-    }
-  }
-})
- 
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
